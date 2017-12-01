f76edbc5e82c7f795afda0741eb45f563086547a
liuyifan3 <liuyifan3@xiaomi.com>
2017-12-01 17:17:24 +0800

    网站定名为缘分天使

--- config/scm_config.php
+++ config/scm_config.php
@@ -4,6 +4,6 @@ $scm_config = array(
     //mysql -uroot -proot -h10.236.103.233 -P3306
     'triangel.db.dsn'                 => 'mysql:host=10.236.103.233;port=3306;dbname=triangel',
     'triangel.db.password'            => 'root',
-    'triangel.db.username'            => 'root',
+    'triangel.db.username'            => '123456',
 
 );
--- views/site/index.php
+++ views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = '爱之神';
+$this->title = '缘分天使';
 ?>
 <style>
     .site-doc-icon li{width: 222px;}
@@ -18,20 +18,41 @@ $this->title = '爱之神';
 
             <ul class="site-doc-icon">
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">丁秀宇</div>
-                    <div class="code">套住你的心</div>
+                    <a target="_blank" href="https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=%E4%B8%81%E7%A7%80%E5%AE%87&rsv_pq=82b1f760000184d8&rsv_t=94bbWWvTlXJforNqLMcrdDsekVpAXuT%2B9pYgoO26ZQ2pmISJlb6kFPlVgx0&rqlang=cn&rsv_enter=1&rsv_sug3=2&rsv_n=2">
+                        <div class="layui-anim" data-anim="layui-anim-up">丁秀宇</div>
+                        <div class="code">套住你的心</div>
+                    </a>
+                </li>
+
+                <li>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">常欢欢</div>
+                        <div class="code">93小鲜肉</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">常欢欢</div>
-                    <div class="code">93小鲜肉</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">王丹丹</div>
+                        <div class="code">我们不一样</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">王丹丹</div>
-                    <div class="code">我们不一样</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘女神</div>
+                        <div class="code">你想认识我吗</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">神秘女神</div>
-                    <div class="code">你想认识我吗</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘女神</div>
+                        <div class="code">你想认识我吗</div>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘女神</div>
+                        <div class="code">你想认识我吗</div>
+                    </a>
                 </li>
             </ul>
 
@@ -41,20 +62,40 @@ $this->title = '爱之神';
 
             <ul class="site-doc-icon">
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">王勇</div>
-                    <div class="code">猥琐大叔</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">王勇</div>
+                        <div class="code">猥琐大叔</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">关东东</div>
-                    <div class="code">就这样执着的爱你</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">关东东</div>
+                        <div class="code">就这样执着的爱你</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">刘伊凡</div>
-                    <div class="code">一胖毁所有</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">刘伊凡</div>
+                        <div class="code">一胖毁所有</div>
+                    </a>
                 </li>
                 <li>
-                    <div class="layui-anim" data-anim="layui-anim-up">神秘男神</div>
-                    <div class="code">你敢认识我吗</div>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘男神</div>
+                        <div class="code">你敢认识我吗</div>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘男神</div>
+                        <div class="code">你敢认识我吗</div>
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" href="">
+                        <div class="layui-anim" data-anim="layui-anim-up">神秘男神</div>
+                        <div class="code">你敢认识我吗</div>
+                    </a>
                 </li>
             </ul>
 
