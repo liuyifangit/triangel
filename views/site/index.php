c5d4c5e287f94cf68eafa3061fb49a73f7dcc806
liuyifan3 <liuyifan3@xiaomi.com>
2017-12-01 15:54:58 +0800

    首页布局

--- views/site/index.php
+++ views/site/index.php
@@ -2,51 +2,62 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = '爱之神';
 ?>
-<div class="site-index">
+<style>
+    .site-doc-icon li{width: 222px;}
+    .site-doc-icon li .layui-anim{width: 150px; height: 150px; line-height: 150px; margin: 0 auto 10px; text-align: center; background-color: #009688; cursor: pointer; color: #fff; border-radius: 50%;}
+</style>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+<div class="layui-tab-item layui-show">
+    <div class="layui-main">
+        <div id="LAY_preview">
+            <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+                <legend>缘分女神</legend>
+            </fieldset>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+            <ul class="site-doc-icon">
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">丁秀宇</div>
+                    <div class="code">套住你的心</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">常欢欢</div>
+                    <div class="code">93小鲜肉</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">王丹丹</div>
+                    <div class="code">我们不一样</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">神秘女神</div>
+                    <div class="code">你想认识我吗</div>
+                </li>
+            </ul>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
+            <fieldset class="layui-elem-field layui-field-title" style="margin-top: 30px;">
+                <legend>缘分男神</legend>
+            </fieldset>
 
-    <div class="body-content">
+            <ul class="site-doc-icon">
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">王勇</div>
+                    <div class="code">猥琐大叔</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">关东东</div>
+                    <div class="code">就这样执着的爱你</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">刘伊凡</div>
+                    <div class="code">一胖毁所有</div>
+                </li>
+                <li>
+                    <div class="layui-anim" data-anim="layui-anim-up">神秘男神</div>
+                    <div class="code">你敢认识我吗</div>
+                </li>
+            </ul>
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
         </div>
 
     </div>
