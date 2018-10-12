4b422104ab67ad8196a9f2cb22471d36466784de
liuyifan3 <liuyifan3@xiaomi.com>
2018-10-12 19:26:47 +0800

    配置文件

--- config/scm_config.php
+++ config/scm_config.php
@@ -1,11 +1,6 @@
 <?php
 
 $scm_config = array(
-    //mysql -uroot -proot -h10.236.103.233 -P3306
-    /*'triangel.db.dsn'                 => 'mysql:host=10.236.103.233;port=3306;dbname=triangel',
-    'triangel.db.password'            => 'root',
-    'triangel.db.username'            => 'root',*/
-
     'triangel.db.dsn'                 => 'mysql:host=localhost;port=3306;dbname=test01',
     'triangel.db.password'            => 'root',
     'triangel.db.username'            => '',
