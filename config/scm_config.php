287de04974ea7b04aeaaa9e5cab9548266a23aee
liuyifangit <479535762@qq.com>
2018-10-12 19:38:27 +0800

    Delete scm_config.php

--- config/scm_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$scm_config = array(
-    'triangel.db.dsn'                 => 'mysql:host=localhost;port=3306;dbname=test01',
-    'triangel.db.password'            => 'root',
-    'triangel.db.username'            => '',
-
-);
