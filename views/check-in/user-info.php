cc67f804777adfebbd49dddb2cd5f2d0ba1bb578
liuyifan3 <liuyifan3@xiaomi.com>
2018-10-13 13:18:16 +0800

    fix

---
+++ views/check-in/user-info.php
@@ -0,0 +1,38 @@
+<form class="layui-form" action="">
+    <div class="layui-form-item">
+        <div class="layui-inline">
+            <label class="layui-form-label">验证手机</label>
+            <div class="layui-input-inline">
+                <input type="tel" name="phone" lay-verify="required|phone" autocomplete="off" class="layui-input" value="<?=$userInfo['phone_num']?>" disabled="disabled"/>
+            </div>
+        </div>
+        <div class="layui-inline">
+            <label class="layui-form-label">姓名</label>
+            <div class="layui-input-inline">
+                <input type="text" name="email"  autocomplete="off" class="layui-input" value="<?=$userInfo['user_name']?>" disabled="disabled"/>
+            </div>
+        </div>
+    </div>
+
+    <div class="layui-form-item">
+        <div class="layui-inline">
+            <label class="layui-form-label">参加人数</label>
+            <div class="layui-input-inline">
+
+                <input type="tel" name="phone" lay-verify="required|phone" autocomplete="off" class="layui-input" value="<?=$userInfo['join_num']?>" disabled="disabled"/>
+            </div>
+        </div>
+        <div class="layui-inline">
+            <label class="layui-form-label">签到次数</label>
+            <div class="layui-input-inline">
+                <input type="text"  autocomplete="off" class="layui-input" value="<?=$userInfo['check_times']?>" disabled="disabled"/>
+            </div>
+        </div>
+    </div>
+
+</form>
+<script  src="http://libs.baidu.com/jquery/1.7.2/jquery.min.js"></script>
+
+<script>
+
+</script>
